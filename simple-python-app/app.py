0a92a11b206ca42a2f11259d4d7dab4bf379d1de
ADITYA KUMAR <93046347+aditya3340@users.noreply.github.com>
2025-02-06 13:35:00 +0530

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! I'm Aditya this is a CI/CD pipeline project'
+    return 'Hello, world! I'm Aditya kumar this is a CI/CD pipeline project'
 
 if __name__ == '__main__':
     app.run()
